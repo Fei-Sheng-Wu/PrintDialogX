845823a6ff626ddee063d088a05f9dc9f2d6f1ed
FeiShengWu <jet20070731@outlook.com>
2020-06-21 14:37:26 -0600

    Add files via upload

---
+++ PrintDialog/PrintDialog/UserControls/NoKeypadDocumentViewer.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Automation.Peers;
+
+namespace PrintDialogX.PrintControl.UserControls
+{
+    internal class NoKeypadDocumentViewer : DocumentViewer
+    {
+        public NoKeypadDocumentViewer()
+        {
+            TextBoxBase.SelectionBrushProperty.OverrideMetadata(typeof(DocumentViewer), new FrameworkPropertyMetadata(Brushes.Red));
+        }
+
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new FrameworkElementAutomationPeer(this);
+        }
+    }
+}
