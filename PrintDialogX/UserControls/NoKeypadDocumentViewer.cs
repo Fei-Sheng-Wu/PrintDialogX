e80d2ea89c7cece21e5351315703b19e156d10fa
FeiShengWu <jet20070731@outlook.com>
2024-02-24 22:34:10 -0600

    Add files via upload

---
+++ PrintDialogX/UserControls/NoKeypadDocumentViewer.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Automation.Peers;
+
+namespace PrintDialogX.PrintControl.UserControls
+{
+    internal class NoKeypadDocumentViewer : DocumentViewer
+    {
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new FrameworkElementAutomationPeer(this);
+        }
+    }
+}
