599cca4dd8b7cb3fabbd4a241d93ffcca48ea22f
FeiShengWu <36951947+Fei-Sheng-Wu@users.noreply.github.com>
2026-02-23 23:41:05 -0500

    Project information updates

--- PrintDialogX/IPrintDialogHost.cs
+++ PrintDialogX/IPrintDialogHost.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
--- PrintDialogX/PrintSettings.cs
+++ PrintDialogX/PrintSettings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Printing;
 using System.ComponentModel;
 
